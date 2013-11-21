b41339701cc09beb83780bcd75315c2f4b321179
Dr.Norton <dimaivanov1@mail.ru>
2013-11-22 01:03:02 +0400

    Начало разработки плейсера для объектов

--- Assets/Editor/MyTypeEditor.cs
+++ Assets/Editor/MyTypeEditor.cs
@@ -1,33 +1,12 @@
 ﻿using Assets.Scripts.Helpers;
 using FruitKiller;
 using UnityEditor;
-using UnityEngine;
-using System.Collections;
 
 
-    [CustomEditor(typeof(Fruit))]
+[CustomEditor(typeof(Entity))]
     public class MyTypeEditor : Editor
     {
-        Fruit m_Instance;
-        PropertyField[] m_fields;
+        
 
-
-        public void OnEnable()
-        {
-            m_Instance = target as Fruit;
-            m_fields = ExposeProperties.GetProperties(m_Instance);
-        }
-
-        public override void OnInspectorGUI()
-        {
-
-            if (m_Instance == null)
-                return;
-
-            this.DrawDefaultInspector();
-
-            ExposeProperties.Expose(m_fields);
-
-        }
     }
 
